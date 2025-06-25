d507eea63b96329787d46a5e04ca8ac672675d86
Dag Wästberg <dwastberg@gmail.com>
2025-06-25 14:16:21 +0200

    city.download methods

--- dtcc_core/io/city_methods/__init__.py
+++ dtcc_core/io/city_methods/__init__.py
@@ -5,6 +5,8 @@ from dtcc_core.model import City, Building, PointCloud, Bounds, Terrain, Geometr
 
 from dtcc_core.builder import register_model_method
 
+from dtcc_core.logging import info, warning, error
+
 import dtcc_core.io as io
 
 from pathlib import Path
@@ -76,3 +78,70 @@ def load_terrain_raster(city: City, path: Union[Path, str]) -> City:
     raster = io.load_raster(path)
     city.add_terrain(raster)
     return City
+
+
+def _get_download_bounds(city_bounds, user_bounds):
+    """
+    Determine the bounds to use for downloading data.
+    """
+
+    if user_bounds is not None and not isinstance(user_bounds, Bounds):
+        raise TypeError("bounds must be a Bounds object")
+
+    if city_bounds is None:
+        if user_bounds is None:
+            error("No bounds found, either set city.bounds or pass in bounds")
+        else:
+            return bounds
+    else:
+        if user_bounds is not None:
+            bounds = city_bounds.intersect(user_bounds)
+        else:
+            bounds = city_bounds
+    return bounds
+
+
+@register_model_method
+def download_footprints(
+    city: City,
+    bounds: Union[Bounds, None] = None,
+) -> City:
+    """
+    Download building footprints from a URL and load them into a City object.
+
+    Args:
+        city (City): The City object to load the footprints into.
+        url (str): The URL to download the building footprints from.
+        user_city_bounds (bool, optional): only load footprints within the set bounds
+
+    Returns:
+        City: The updated City object with the loaded footprints.
+    """
+
+    download_bounds = _get_download_bounds(city.bounds, bounds)
+
+    footprints = io.data.download_footprints(bounds=download_bounds)
+    city.add_buildings(footprints)
+    city.calculate_bounds()
+    return city
+
+
+@register_model_method
+def download_pointcloud(city: City, bounds: Union[Bounds, None] = None) -> City:
+    """
+    Download pointcloud from a URL and load it into a City object.
+
+    Args:
+        city (City): The City object to load the pointcloud into.
+        bounds (Bounds, optional): The bounds to filter the pointcloud.
+
+    Returns:
+        City: The updated City object with the loaded pointcloud.
+    """
+
+    download_bounds = _get_download_bounds(city.bounds, bounds)
+
+    pc = io.data.download_pointcloud(bounds=download_bounds)
+    city.add_point_cloud(pc)
+    city.calculate_bounds()
+    return city
---
+++ sandbox/city_download.py
@@ -0,0 +1,14 @@
+import dtcc_core
+from dtcc_core.model import City, Bounds
+import dtcc_viewer
+
+city = City()
+h = 500.0
+bounds = Bounds(319891, 6399790, 319891 + h, 6399790 + h)
+
+city.bounds = bounds
+
+city.download_footprints()
+city.download_pointcloud()
+city.build_lod1_buildings()
+city.view()
