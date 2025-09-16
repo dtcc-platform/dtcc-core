6498a54be9adc737a1987ab678dfa28b5f113c29
Dag Wästberg <dwastberg@gmail.com>
2025-09-16 14:40:16 +0200

    remove unnecessary bounds args

--- sandbox/surface_tiling/surface_tiling.py
+++ sandbox/surface_tiling/surface_tiling.py
@@ -14,8 +14,8 @@ bounds = Bounds(319891, 6399790, 319891 + h, 6399790 + h, 0, 200)
 city.bounds = bounds
 
 # Download pointcloud and building footprints
-city.download_pointcloud(bounds=bounds, filter_on_z_bounds=True)
-city.download_footprints(bounds=bounds)
+city.download_pointcloud(filter_on_z_bounds=True)
+city.download_footprints()
 
 city.building_heights_from_pointcloud()
 
